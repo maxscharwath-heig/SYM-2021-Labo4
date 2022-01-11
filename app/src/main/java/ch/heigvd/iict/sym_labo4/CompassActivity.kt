44d60e25fdd0a7070b27945538e336f1bfecb67e
nicrausaz <n.crausaz99@gmail.com>
2022-01-11 11:19:13 +0100

    Base of BLE action panel

--- app/src/main/java/ch/heigvd/iict/sym_labo4/CompassActivity.kt
+++ app/src/main/java/ch/heigvd/iict/sym_labo4/CompassActivity.kt
@@ -1,7 +1,12 @@
 package ch.heigvd.iict.sym_labo4
 
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.opengl.GLSurfaceView
 import android.os.Bundle
+import android.util.Log
 import android.view.Window
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
@@ -13,18 +18,28 @@ import ch.heigvd.iict.sym_labo4.gl.OpenGLRenderer
  * Updated by fabien.dutoit on 06.11.2020
  * (C) 2016 - HEIG-VD, IICT
  */
-class CompassActivity : AppCompatActivity() {
+class CompassActivity : AppCompatActivity(), SensorEventListener {
 
     //opengl
     private lateinit var opglr: OpenGLRenderer
     private lateinit var m3DView: GLSurfaceView
+    private lateinit var sensorManager: SensorManager
+    private lateinit var accelerometerSensor: Sensor
+    private lateinit var magnetometerSensor: Sensor
+
+    private lateinit var accData: FloatArray
+    private lateinit var magnData: FloatArray
+    private lateinit var rotMatrix: FloatArray
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // we need fullscreen
         requestWindowFeature(Window.FEATURE_NO_TITLE)
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
 
         // we initiate the view
         setContentView(R.layout.activity_compass)
@@ -38,16 +53,38 @@ class CompassActivity : AppCompatActivity() {
         //init opengl surface view
         m3DView.setRenderer(opglr)
 
+        // Register to sensors
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        magnetometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
+        accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+        // Init arrays
+        accData = FloatArray(3)
+        magnData = FloatArray(3)
+        rotMatrix = FloatArray(16)
     }
 
-    /*
-        TODO
-        your activity need to register to accelerometer and magnetometer sensors' updates
-        then you may want to call
-        opglr.swapRotMatrix()
-        with the 4x4 rotation matrix, every time a new matrix is computed
-        more information on rotation matrix can be found online:
-        https://developer.android.com/reference/android/hardware/SensorManager.html#getRotationMatrix(float[],%20float[],%20float[],%20float[])
-    */
+    override fun onSensorChanged(event: SensorEvent) {
 
+        when (event.sensor.type) {
+            Sensor.TYPE_ACCELEROMETER  -> accData  = event.values
+            Sensor.TYPE_MAGNETIC_FIELD -> magnData = event.values
+        }
+
+        rotMatrix = opglr.swapRotMatrix(rotMatrix)
+        SensorManager.getRotationMatrix(rotMatrix, null, accData, magnData);
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {}
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.registerListener(this, magnetometerSensor, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
 }
